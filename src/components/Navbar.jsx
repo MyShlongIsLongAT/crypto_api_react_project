b4dd1880c0efa73098ed6e2d7da1b8158c60bd4c
HavidDartmann <david.hartmann@student.htldornbirn.at>
2022-02-22 11:25:52 +0100

    Link on Dropw Down Menu not working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,114 @@
-import React from 'react';
-import {
-	AppBar,
-	Toolbar,
-	IconButton,
-	Button,
-	Box,
-	Container,
-} from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
 
-const Navbar = () => {
+import Button from '@mui/material/Button';
+
+import MenuItem from '@mui/material/MenuItem';
+
+const pages = [
+	{ name: 'Homepage', link: '/' },
+	{ name: 'Cryptocurrencies', link: '/cryptocurrencies' },
+];
+
+const ResponsiveAppBar = () => {
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+
 	return (
-		<>
-			<Box sx={{ flexGrow: 1 }}>
-				<AppBar position="static">
-					<Toolbar>
+		<AppBar position="static">
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<Typography
+						variant="h6"
+						noWrap
+						component="div"
+						sx={{ mr: 2, display: { xs: 'none', sm: 'flex' } }}
+					>
+						LOGO
+					</Typography>
+
+					<Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'none' } }}>
 						<IconButton
 							size="large"
-							edge="start"
+							aria-label="account of current user"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={handleOpenNavMenu}
 							color="inherit"
-							aria-label="menu"
-							sx={{ mr: 2 }}
 						>
-							<HomeIcon />
+							<MenuIcon />
 						</IconButton>
-						<Button
-							href="/"
-							varient="text"
-							color="inherit"
-							sx={{ mr: 2, display: 'block' }}
+
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: 'bottom',
+								horizontal: 'left',
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'left',
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: 'block', sm: 'none' },
+							}}
 						>
-							Homepage
-						</Button>
+							{pages.map((page) => (
+								<MenuItem key={page.name} onClick={handleCloseNavMenu}>
+									<Typography textAlign="center">
+										{page.name}
+									</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+
+					<Typography
+						variant="h6"
+						noWrap
+						component="div"
+						sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'none' } }}
+					>
+						LOGO
+					</Typography>
+
+					<Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'flex' } }}>
+						{pages.map((page) => (
+							<Button
+								key={page.name}
+								href={page.link}
+								onClick={handleCloseNavMenu}
+								sx={{ my: 2, color: 'white', display: 'block' }}
+							>
+								{page.name}
+							</Button>
+						))}
+					</Box>
+
+					<Box sx={{ flexGrow: 0 }}>
 						<Button
-							href="/cryptocurrencies"
-							varient="text"
+							variant="outlined"
 							color="inherit"
-							sx={{ display: 'block' }}
+							sx={{ mr: 2, minWidth: 90 }}
 						>
-							Cryptocurrencies
-						</Button>
-						<Container sx={{ flexGrow: 1 }}></Container>
-						<Button variant="outlined" color="inherit" sx={{ mr: 2 }}>
 							Sign In
 						</Button>
 						<Button
@@ -51,11 +118,10 @@ const Navbar = () => {
 						>
 							Sign Up
 						</Button>
-					</Toolbar>
-				</AppBar>
-			</Box>
-		</>
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
 	);
 };
-
-export default Navbar;
+export default ResponsiveAppBar;
