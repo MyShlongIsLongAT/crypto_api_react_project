704156161a86bf93da115dbae8bf059daed159b8
MyShlongIsLongAT <florian_breuker@outlook.com>
2022-02-24 21:10:01 +0100

    Merge branch 'main' of https://github.com/MyShlongIsLongAT/crypto_api_react_project

--- src/pages/Cryptocurrencies.jsx
+++ src/pages/Cryptocurrencies.jsx
@@ -1,16 +1,11 @@
 import React from "react";
-import { CoinProvider } from "../components/coinContext";
 import CoinList from "../components/coinList";
-import styles from "./Cryptocurrencies.module.css"
+import styles from "./Cryptocurrencies.module.css";
 
 const Cryptocurrencies = () => {
   return (
     <div className={styles.coinContent}>
-      <>
-        <CoinProvider>
-          <CoinList />
-        </CoinProvider>
-      </>
+      <CoinList />
     </div>
   );
 };
