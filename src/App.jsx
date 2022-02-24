1ba92569774b538c1afe27866c000eb53cd38765
MyShlongIsLongAT <florian_breuker@outlook.com>
2022-02-24 21:10:29 +0100

    fixed some kinda merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { Navbar } from './components';
-import { Homepage, Cryptocurrencies } from './pages';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components";
+import { Homepage, Cryptocurrencies } from "./pages";
+import "./App.css";
+import { CoinProvider } from "./components/coinContext";
 
 const App = () => {
-	return (
-		<div className="app">
-			<div className="navbar">
-				<Navbar />
-			</div>
-			<div className="main">
-				<div className="routes">
-					<Routes>
-						<Route exact path="/" element={<Homepage />} />
-						<Route
-							exact
-							path="/cryptocurrencies"
-							element={<Cryptocurrencies />}
-						/>
-					</Routes>
-				</div>
-			</div>
-		</div>
-	);
-
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <CoinProvider>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+            </Routes>
+          </div>
+        </CoinProvider>
+      </div>
+    </div>
+  );
 };
 
 export default App;
