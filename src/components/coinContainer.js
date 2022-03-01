9556d7e8640f6c2663a135377cf9082b3885b4b7
MyShlongIsLongAT <florian_breuker@outlook.com>
2022-03-01 15:07:04 +0100

    added working grid

--- src/components/coinContainer.js
+++ src/components/coinContainer.js
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./coinContainer.module.css";
 
-const coinContainer = (props) => {
+const CoinContainer = (props) => {
+  const [matches, setMatches] = useState(
+    window.matchMedia("(max-width: 1100px)").matches
+  );
+
+  useEffect(() => {
+    window
+      .matchMedia("(max-width: 1100px)")
+      .addEventListener("change", (e) => setMatches(e.matches));
+  }, []);
+
   return (
     <div className={styles.wholeContainer}>
-      <table>
-        <tr>
-          <td width="100">
-            <img src={props.iconUrl} width="50px" alt="Logo" />
-          </td>
-          <td width="270">
-            <h3>{props.name}</h3>
-            <span>{props.symbol}</span>
-          </td>
-          <td width="100">
-            <h5>{"$ " + parseFloat(props.price).toFixed(5)}</h5>
-          </td>
-        </tr>
-      </table>
+      {matches && (
+        <table style={{width:"100%"}}>
+          <tr>
+            <td width="30%">
+              <img src={props.iconUrl} width="30px" alt="Logo" />
+            </td>
+            <td width="30%">
+              <h3>{props.symbol}</h3>
+            </td>
+            <td width="40%">
+              <h5>{"$ " + parseFloat(props.price).toFixed(5)}</h5>
+            </td>
+          </tr>
+        </table>
+      )}
+      {!matches && (
+        <table>
+          <tr>
+            <td width="100">
+              <img src={props.iconUrl} width="50px" alt="Logo" />
+            </td>
+            <td width="260">
+              <h3>{props.name}</h3>
+              <span>{props.symbol}</span>
+            </td>
+            <td width="110">
+              <h5>{"$ " + parseFloat(props.price).toFixed(5)}</h5>
+            </td>
+          </tr>
+        </table>
+      )}
     </div>
   );
 };
 
-export default coinContainer;
+export default CoinContainer;
--- src/components/coinList.jsx
+++ src/components/coinList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import CoinContainer from "./coinContainer";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
@@ -7,16 +7,60 @@ import { coinContext } from "../services/coinContext";
 const CoinList = () => {
   const data = useContext(coinContext);
 
+  const [matches, setMatches] = useState(
+    window.matchMedia("(max-width: 1046px)").matches
+  );
+
+  useEffect(() => {
+    window
+      .matchMedia("(max-width: 1046px)")
+      .addEventListener("change", (e) => setMatches(e.matches));
+  }, []);
+
   return (
     <div>
-      <Box sx={{ flexGrow: 1 }}>
+      {matches && (
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ s: 3, xs: 3, sm: 8, md: 12 }}
+          >
+            {data.coins.map((coin, index) => (
+              <Grid
+                item
+                xs={3}
+                sm={4}
+                md={4}
+                key={index}
+                
+              >
+                <CoinContainer
+                  price={coin.price}
+                  iconUrl={coin.iconUrl}
+                  symbol={coin.symbol}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      )}
+      {!matches && (
+        <Box sx={{ flexGrow: 1 }}>
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
           columns={{ s: 3, xs: 3, sm: 8, md: 12 }}
         >
           {data.coins.map((coin, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index} sx={{minWidth:490}}>
+            <Grid
+              item
+              xs={3}
+              sm={4}
+              md={4}
+              key={index}
+              
+            >
               <CoinContainer
                 name={coin.name}
                 price={coin.price}
@@ -27,6 +71,7 @@ const CoinList = () => {
           ))}
         </Grid>
       </Box>
+      )}
     </div>
   );
 };
