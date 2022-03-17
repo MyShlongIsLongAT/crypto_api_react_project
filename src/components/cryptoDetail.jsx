0a6dc596c4124d80fa54d2fa887716fe5bbbc0e1
HavidDartmann <david.hartmann@student.htldornbirn.at>
2022-03-17 11:05:09 +0100

    Zugriff auf einzelne Coins mit coin.state

--- src/components/cryptoDetail.jsx
+++ src/components/cryptoDetail.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 const CryptoDetail = () => {
-	const location = useLocation();
-	console.log(location);
-	return <div>Coin</div>;
+	const coin = useLocation();
+	console.log(coin);
+	return <img src={coin.state.iconUrl}></img>;
 };
 
 export default CryptoDetail;
