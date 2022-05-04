3fe842edf8a27d0aa37476a134e8a497b89f6daa
HavidDartmann <david.hartmann@student.htldornbirn.at>
2022-05-04 21:24:29 +0200

    bugfix

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -64,7 +64,7 @@ export default function ForgotPassword() {
 					</Button>
 					<Grid container justifyContent="flex-end">
 						<Grid item>
-							<Link href="/signup" variant="body2">
+							<Link href="/signin" variant="body2">
 								Sign In
 							</Link>
 						</Grid>
